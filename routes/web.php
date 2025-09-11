bb68ad0e74e6e5257f6ba2583604be9d63a8c983
vic657 <jumav1211@gmail.com>
2025-09-12 01:04:14 +0300

    restore route

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/health', fn() => response()->json(['status' => 'ok']));
 
-// TEMP: Root test route
-Route::get('/', function () {
-    return 'App is running!';
-});
-
 // Catch-all route (for React build)
 Route::get('/{any}', function () {
     return File::get(public_path('index.html'));
 })->where('any', '.*');
-
