6023218f582b177e794ca7d606e6a1b114d2a2a0
vic657 <jumav1211@gmail.com>
2025-09-11 21:13:28 +0300

    new dep

--- frontend/src/Axios.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
- // Laravel API URL
-  withCredentials: true,
-});
-
-export default instance;
